2d5149e2904102ddc2291c5e48afc1f3e4245e4f
TrikaMit <trikamitra@gmail.com>
2018-07-28 13:56:15 -0400

    sequelize

--- server.js
+++ server.js
@@ -3,27 +3,28 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080;
-// var db = require("./models");
+var db = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // //LCBO API
-// $.ajax({
-//     url: 'https://lcboapi.com/products?q=' + 'merlot',
-//     method: 'GET',
-//     headers: {
-//         'Authorization': `Token ${process.env.ACCESS_KEY}`
-//     }
-// }).then(response => {
-//     console.log(response)
-// });
+var query = "this will be our query"
+$.ajax({
+    url: 'https://lcboapi.com/products?q=' + query,
+    method: 'GET',
+    headers: {
+        'Authorization': `Token ${process.env.ACCESS_KEY}`
+    }
+}).then(response => {
+    console.log(response)
+});
 
 console.log(process.env.ACCESS_KEY)
 
-// db.sequelize.sync({ force: true }).then(function () {
-//     app.listen(PORT, function () {
-//         console.log("App listening on PORT " + PORT);
-//     });
-// });
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
+});
