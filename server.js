c8ba903132200e7b86018cf01102a6625e3c2673
TrikaMit <trikamitra@gmail.com>
2018-07-28 14:07:03 -0400

    request

--- server.js
+++ server.js
@@ -3,28 +3,21 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080;
-var db = require("./models");
+var request = require('request');
+// var db = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
-
-// //LCBO API
-var query = "this will be our query"
-$.ajax({
-    url: 'https://lcboapi.com/products?q=' + query,
-    method: 'GET',
-    headers: {
-        'Authorization': `Token ${process.env.ACCESS_KEY}`
-    }
-}).then(response => {
-    console.log(response)
+ 
+var query = "Mill+Street+Organic"
+request(`https://lcboapi.com/products?access_key=${process.env.ACCESS_KEY}&q=${query}`, { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body.result[0]);
 });
 
-console.log(process.env.ACCESS_KEY)
-
-db.sequelize.sync({ force: true }).then(function () {
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
-});
+// db.sequelize.sync({ force: true }).then(function () {
+//     app.listen(PORT, function () {
+//         console.log("App listening on PORT " + PORT);
+//     });
+// });
