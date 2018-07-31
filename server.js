8902c0aee2b7d7da09988e3a689999ad8aa2bc44
leandra <leandra.reid@gmail.com>
2018-07-31 19:48:58 -0400

    updates to search result page

--- server.js
+++ server.js
@@ -9,12 +9,14 @@ var request = require('request');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
- 
+
 var query = "Mill+Street+Organic"
 request(`https://lcboapi.com/products?access_key=${process.env.ACCESS_KEY}&q=${query}`, { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   console.log(body.result[0]);
 });
+
+
 
 // db.sequelize.sync({ force: true }).then(function () {
 //     app.listen(PORT, function () {
