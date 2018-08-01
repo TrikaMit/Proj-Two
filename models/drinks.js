4466b672b6f575af2e2bb91fb5c85d8343998dab
TrikaMit <trikamitra@gmail.com>
2018-07-31 20:45:17 -0400

    models

--- models/drinks.js
+++ models/drinks.js
@@ -1,6 +1,22 @@
 module.exports = function (sequelize, DataTypes) {
     var Drinks = sequelize.define("Drinks", {
+      //Drink will have a title (User/LCBO API), type (LCBO API), rating (LCBO API), and review (User input)
       title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      rating: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      review: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
@@ -12,13 +28,12 @@ module.exports = function (sequelize, DataTypes) {
     Drinks.associate = function (models) {
       // We're saying that a Post should belong to an Author
       // A Post can't be created without an Author due to the foreign key constraint
-      Post.belongsTo(models.User, {
+      Drinks.belongsTo(models.User, {
         foreignKey: {
           allowNull: false
         }
       });
     };
-  
     return Drinks;
   };
   
