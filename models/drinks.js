8d5aa54892f3879c6b024743761b7d89268b2d73
TrikaMit <trikamitra@gmail.com>
2018-08-02 18:36:39 -0400

    missing comma

--- models/drinks.js
+++ models/drinks.js
@@ -22,7 +22,7 @@ module.exports = function (sequelize, DataTypes) {
         validate: {
           len: [1]
         }
-      }
+      },
       url: {
         type: DataTypes.STRING,
         allowNull: false,
