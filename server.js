b9cd8d8a7805e0bd39ebc827cd320ba5fd40dfcb
TrikaMit <trikamitra@gmail.com>
2018-08-09 03:45:59 -0400

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express();
 var PORT = process.env.PORT || 8080
-var request = require('request')
 var db = require('./models');
 
 app.use(bodyParser.urlencoded({ extended: true }))
